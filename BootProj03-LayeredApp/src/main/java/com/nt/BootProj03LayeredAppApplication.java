b5db5492d73003aac3a788e0eebcb5cfb189a24d
Nazeer <nazeersd.java@gmail.com>
2022-06-21 02:47:26 +0530

    Spring Boot Layered Application

---
+++ BootProj03-LayeredApp/src/main/java/com/nt/BootProj03LayeredAppApplication.java
@@ -0,0 +1,31 @@
+package com.nt;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.nt.controller.PayrollOperationsController;
+import com.nt.model.Employee;
+
+@SpringBootApplication
+public class BootProj03LayeredAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(BootProj03LayeredAppApplication.class, args);
+		PayrollOperationsController controller = ctx.getBean("payroll", PayrollOperationsController.class);
+		try {
+			List<Employee> allEmployeesByDesgs = controller.showAllEmployeesByDesgs("CLERK", "MANAGER", "SALESMAN");
+			allEmployeesByDesgs.forEach(emp -> {
+				System.out.println(emp);
+			});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		((ConfigurableApplicationContext) ctx).close();
+	}
+
+}
