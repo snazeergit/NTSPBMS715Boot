a2e2394e095c77078f642e537b341bc481fba9c3
Nazeer <nazeersd.java@gmail.com>
2022-06-22 01:28:49 +0530

    LayeredApp-c3p0

---
+++ BootProj03-LayeredApp-c3p0/src/main/java/com/nt/BootProj03LayeredAppApplication.java
@@ -0,0 +1,64 @@
+package com.nt;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.nt.controller.PayrollOperationsController;
+import com.nt.model.Employee;
+
+@SpringBootApplication
+public class BootProj03LayeredAppApplication {
+
+	@Autowired
+	private Environment env;
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(BootProj03LayeredAppApplication.class, args);
+		PayrollOperationsController controller = ctx.getBean("payroll", PayrollOperationsController.class);
+		try {
+			List<Employee> allEmployeesByDesgs = controller.showAllEmployeesByDesgs("CLERK", "MANAGER", "SALESMAN");
+			allEmployeesByDesgs.forEach(emp -> {
+				System.out.println(emp);
+			});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		((ConfigurableApplicationContext) ctx).close();
+	}
+
+	/*//database details hard coded
+	@Bean(name = "c3p0")
+	public ComboPooledDataSource createC3P0DS() throws Exception {
+		ComboPooledDataSource c3p0DS = new ComboPooledDataSource();
+		c3p0DS.setDriverClass("oracle.jdbc.OracleDriver");
+		c3p0DS.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		c3p0DS.setUser("System");
+		c3p0DS.setPassword("VMware1");
+		c3p0DS.setInitialPoolSize(10);
+		c3p0DS.setMaxPoolSize(100);
+		return c3p0DS;
+	}*/
+
+	//Reading database details from Properties file using Environment variable
+	@Bean(name = "c3p0")
+	public ComboPooledDataSource createC3P0DS() throws Exception {
+		ComboPooledDataSource c3p0DS = new ComboPooledDataSource();
+		c3p0DS.setDriverClass(env.getProperty("spring.datasource.driver-class-name"));
+		c3p0DS.setJdbcUrl(env.getProperty("spring.datasource.url"));
+		c3p0DS.setUser(env.getProperty("spring.datasource.username"));
+		c3p0DS.setPassword(env.getProperty("spring.datasource.password"));
+		c3p0DS.setInitialPoolSize(Integer.parseInt(env.getRequiredProperty("c3p0.minSize")));
+		c3p0DS.setMaxPoolSize(Integer.parseInt(env.getRequiredProperty("c3p0.maxSize")));
+		return c3p0DS;
+	}
+
+}
---
+++ BootProj03-LayeredApp-c3p0/src/main/java/com/nt/persistance/EmployeeDAOImpl.java
@@ -0,0 +1,58 @@
+package com.nt.persistance;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.nt.model.Employee;
+
+@Repository("empDAO")
+public class EmployeeDAOImpl implements IEmployeeDAO {
+
+	private static final String GET_EMPS_BY_DESGS = "SELECT ENO,ENAME,JOB,SAL,DEPTNO FROM EMPLOYEE WHERE JOB IN(?,?,?) ORDER BY JOB";
+
+	@Autowired
+	private DataSource ds;
+
+	@Override
+	public List<Employee> getEmployeesByDesgs(String desg1, String desg2, String desg3) throws Exception {
+
+		System.out.println("EmployeeDAOImpl.getEmployeesByDesgs(): "+ds.getClass());
+
+		List<Employee> empList = null;
+		Connection con = ds.getConnection();
+		PreparedStatement ps = con.prepareStatement(GET_EMPS_BY_DESGS);
+
+		try (con; ps) {
+			ps.setString(1, desg1);
+			ps.setString(2, desg2);
+			ps.setString(3, desg3);
+			ResultSet rs = ps.executeQuery();
+			try (rs) {
+				empList = new ArrayList<>();
+				while (rs.next()) {
+					Employee emp = new Employee();
+					emp.setEno(rs.getInt(1));
+					emp.setEname(rs.getString(2));
+					emp.setJob(rs.getString(3));
+					emp.setSalary(rs.getDouble(4));
+					emp.setDeptNo(rs.getInt(5));
+					empList.add(emp);
+				}
+			}
+		} catch (SQLException se) {
+			throw se;
+		} catch (Exception e) {
+			throw e;
+		}
+		return empList;
+	}
+}
