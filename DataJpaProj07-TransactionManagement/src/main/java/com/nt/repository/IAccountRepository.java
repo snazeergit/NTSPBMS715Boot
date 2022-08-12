ea68023c4e781812059eaf8e14190ac13e71fd16
Nazeer <nazeersd.java@gmail.com>
2022-08-13 02:14:19 +0530

    Tx Mgmt

--- DataJpaProj07-TransactionManagement/src/main/java/com/nt/repository/IAccountRepository.java
+++ DataJpaProj07-TransactionManagement/src/main/java/com/nt/repository/IAccountRepository.java
@@ -8,24 +8,10 @@ import com.nt.entity.Account;
 
 public interface IAccountRepository extends JpaRepository<Account, Long> {
 
-	//Positional parameters
-	//@Query(nativeQuery = true, value = "update Account set amount=amount-? where acno=?")
-
-	//Ordinal Positional parameters
-	//@Query(nativeQuery = true, value = "update Account set amount=amount-?1 where acno=?2")
-
-	//Named parameters
 	@Query("update Account set amount=amount-:amt where acno=:srcAcno")
 	@Modifying
 	public int withdrawMoney(Long srcAcno, Double amt);
 
-	//Positional parameters
-	//@Query(nativeQuery = true, value = "update Account set amount=amount+? where acno=?")
-
-	//Ordinal Positional parameters
-	//@Query(nativeQuery = true, value = "update Account set amount=amount+?1 where acno=?2")
-
-	//Named parameters
 	@Query("update Account set amount=amount+:amt where acno=:srcAcno")
 	@Modifying
 	public int depositMoney(Long srcAcno, Double amt);
