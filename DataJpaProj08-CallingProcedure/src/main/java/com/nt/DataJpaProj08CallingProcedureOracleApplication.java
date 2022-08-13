cb9b86d445efc911e3c9df57203f32a95bf6dc8d
Nazeer <nazeersd.java@gmail.com>
2022-08-13 07:30:10 +0530

    CallingProcedure-Oracle

--- DataJpaProj08-CallingProcedure/src/main/java/com/nt/DataJpaProj08CallingProcedureOracleApplication.java
+++ DataJpaProj08-CallingProcedure/src/main/java/com/nt/DataJpaProj08CallingProcedureOracleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataJpaProj08CallingProcedureApplication {
+public class DataJpaProj08CallingProcedureOracleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataJpaProj08CallingProcedureApplication.class, args);
+		SpringApplication.run(DataJpaProj08CallingProcedureOracleApplication.class, args);
 	}
 
 }
--- DataJpaProj08-CallingProcedure/src/test/java/com/nt/DataJpaProj08CallingProcedureOracleApplicationTests.java
+++ DataJpaProj08-CallingProcedure/src/test/java/com/nt/DataJpaProj08CallingProcedureOracleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DataJpaProj08CallingProcedureApplicationTests {
+class DataJpaProj08CallingProcedureOracleApplicationTests {
 
 	@Test
 	void contextLoads() {
