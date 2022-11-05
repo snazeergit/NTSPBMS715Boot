7145c1bfcf63ef6856787eb082e18f954c72f86e
Nazeer <nazeersd.java@gmail.com>
2022-11-05 19:09:06 +0530

    MVCBootProj16-Mini_Project-CURD_Operations-Thymeleaf

---
+++ MVCBootProj16-Mini_Project-CURD_Operations-Thymeleaf/src/main/java/com/nt/service/IEmployeeMgmtService.java
@@ -0,0 +1,11 @@
+package com.nt.service;
+
+import com.nt.model.Employee;
+
+public interface IEmployeeMgmtService {
+	public Iterable<Employee> getAllEmployees();
+	public String registerEmployee(Employee emp);
+	public Employee getEmployeeByNo(Integer eno);
+	public String updateEmployee(Employee emp);
+	public String deleteEmployeeByNo(Integer eno);
+}
