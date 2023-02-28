4e0df2f9793644e4d486df9aa84d2d4f80ddf45e
snazeer <snazeer@snazeer-z02>
2023-02-28 18:30:05 +0530

    All 3 approaches updated in BatchConfig class

--- SpringBootBatchProj02-CSVToDBTable/src/main/java/com/nt/config/BatchConfig.java
+++ SpringBootBatchProj02-CSVToDBTable/src/main/java/com/nt/config/BatchConfig.java
@@ -10,7 +10,9 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -38,7 +40,8 @@ public class BatchConfig {
 	@Autowired
 	private DataSource ds;
 
-	@Bean(name = "ffiReader")
+	//Approach 1:
+	/*@Bean(name = "ffiReader")
 	public FlatFileItemReader<Employee> createFFIReader() {
 		//create Reader object
 		FlatFileItemReader<Employee> reader = new FlatFileItemReader<Employee>();
@@ -60,7 +63,7 @@ public class BatchConfig {
 		reader.setLineMapper(lineMapper);
 		return reader;
 	}
-
+	
 	@Bean(name = "jbiWriter")
 	public JdbcBatchItemWriter<Employee> createJBIWriter() {
 		//create JdbcBatchItemWriter
@@ -74,8 +77,68 @@ public class BatchConfig {
 		//set SourceProvider to writer object
 		writer.setItemSqlParameterSourceProvider(sourceProvider);
 		return writer;
+	}*/
+
+	//Approach 2:
+	/*@Bean(name = "ffiReader")
+	public FlatFileItemReader<Employee> createFFIReader() {
+		FlatFileItemReader<Employee> reader = new FlatFileItemReader<Employee>();
+		reader.setResource(new ClassPathResource("Employee_Info.csv"));
+		reader.setLineMapper(new DefaultLineMapper<Employee>() { //anonymous sub class
+			{//instance block
+				//method called from instance block
+				setLineTokenizer(new DelimitedLineTokenizer() {
+					{
+						//methods called from instance block
+						setDelimiter(",");
+						setNames("empno", "ename", "eadd", "salary");
+					}
+				});
+				//method called from instance block
+				setFieldSetMapper(new BeanWrapperFieldSetMapper<Employee>() {
+					{
+						//method called from instance block
+						setTargetType(Employee.class);
+					}
+				});
+			}//instance block close
+		}//anonymous sub class close
+		);
+		return reader;
+	}
+	
+	@Bean(name = "jbiWriter")
+	public JdbcBatchItemWriter<Employee> createJBIWriter() {
+		//create JdbcBatchItemWriter
+		JdbcBatchItemWriter<Employee> writer = new JdbcBatchItemWriter<Employee>() {//anonymous sub class
+			{//instance block
+				//methods called from instance block
+				setDataSource(ds);
+				setSql("INSERT INTO BATCH_EMPLOYEE_INFO VALUES(:empno,:ename,:eadd,:salary,:grossSalary,:netSalary)");
+				setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Employee>());
+			}//instance block close
+		}//anonymous sub class close
+		;
+		return writer;
+	}*/
+
+	//Approach 3:
+	//By using ReadyMade Builder class
+	@Bean(name = "ffiReader")
+	public FlatFileItemReader<Employee> createFFIReader() {
+		return new FlatFileItemReaderBuilder<Employee>().name("file-reader").resource(new ClassPathResource("Employee_Info.csv"))
+				.delimited().names("empno", "ename", "eadd", "salary").targetType(Employee.class).build();
 	}
 
+	//Using Builder class
+	@Bean(name = "jbiWriter")
+	public JdbcBatchItemWriter<Employee> createJBIWriter() {
+		return new JdbcBatchItemWriterBuilder<Employee>().dataSource(ds)
+				.sql("INSERT INTO BATCH_EMPLOYEE_INFO VALUES(:empno,:ename,:eadd,:salary,:grossSalary,:netSalary)")
+				.beanMapped().build();
+	}
+
+	
 	@Bean(name = "step1")
 	public Step createStep1() {
 		return stepBuilderFactory.get("step1").<Employee, Employee>chunk(5).reader(createFFIReader())
