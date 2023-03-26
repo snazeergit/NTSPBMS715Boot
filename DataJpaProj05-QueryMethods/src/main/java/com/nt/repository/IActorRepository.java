55c63e0196395d1686e071b8d1bd25723b031854
snazeer <snazeer@snazeer-z02>
2023-03-27 04:43:14 +0530

    updated

--- DataJpaProj05-QueryMethods/src/main/java/com/nt/repository/IActorRepository.java
+++ DataJpaProj05-QueryMethods/src/main/java/com/nt/repository/IActorRepository.java
@@ -7,19 +7,30 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.nt.entity.Actor;
 
 @Transactional
 public interface IActorRepository extends JpaRepository<Actor, Integer> {
 
-	//-------------1. Entity Operation/Query-------------------------------------------
-	/*@Query(" from Actor where category=?1")
+	//-------------1. Entity Operation/Query-----------------
+	/*
+	 @Query(" from Actor where category=?1")
 	@Query("from Actor a where a.category=?1")
-	@Query("select a from com.nt.entity.Actor a where a.category=?1")*/
+	@Query("select a from com.nt.entity.Actor a where a.category=?1")
 	@Query("from Actor where category=:role")
-	public List<Actor> searchActorsByCategory(String role);
-
+	public List<Actor> searchActorsByCategory(String role);*/
+											//	(or)
+	@Query("from Actor where category=:role")
+	public List<Actor> searchActorsByCategory(@Param("role") String category);
+	
+	/*
+	 * If the @Query method parameter name is matching with JPQL-HQL query's named parameter name then 
+	 * giving @Param is optional while declaring @Query method in the Repository interface.
+	 * For this we should enable following setting in Eclipse.
+	 */
+	
 	/*
 	//This will sort the result
 	@Query("from Actor where mobileNo in(?1,?2,?3,?4)")
@@ -32,7 +43,7 @@ public interface IActorRepository extends JpaRepository<Actor, Integer> {
 	@Query("from Actor where mobileNo in(?1,?4,?3,?2) order by aname")
 	public Iterable<Actor> findActorsByMobileNo(long no1, long no2, long no3, long no4);
 
-	//------------2. Scalar Operation/Query selecting specific multuple column values-------------------
+	//------------2. Scalar Operation/Query selecting specific multiple column values-------------------
 	@Query("select aid, aname, category from Actor where category in(:cat1,:cat2) order by aname")
 	public List<Object[]> findActorsDataByCategories(String cat1, String cat2);
 
@@ -40,7 +51,7 @@ public interface IActorRepository extends JpaRepository<Actor, Integer> {
 	@Query("select aname from Actor where mobileNo=?1")
 	public String findActorNameByMobileNo(long no);
 
-	//--------------4. Executing Mulitple Aggregate functions using @Query-----------------
+	//--------------4. Executing Multiple Aggregate functions using @Query-----------------
 	@Query("select count(*), max(aid), min(aid), sum(aid), avg(aid) from Actor ")
 	public Object getActorsAggregateData();
 
